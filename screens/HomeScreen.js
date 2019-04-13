10e42595b434620dcc1e8008129f7fc349c9f43f
Ben Mitchell <dezyh@outlook.com>
2019-04-13 17:44:10 +1000

    hardcode route demo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,9 +7,11 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
-import { MapView } from "expo";
+import { MapView, Polyline } from "expo";
+import MapViewDirections from 'react-native-maps-directions';
 
 export default class HomeScreen extends React.Component {
+
   static navigationOptions = {
     header: null
   };
@@ -24,7 +26,20 @@ export default class HomeScreen extends React.Component {
           latitudeDelta: 0.05,
           longitudeDelta: 0.02
         }}
+      >
+
+      <MapViewDirections
+        origin={{latitude: -37.801725, longitude: 144.958462}}
+        destination={{latitude: -37.801725, longitude: 144.958462}}
+        waypoints={[{latitude: -37.800406, longitude: 144.963822}, {latitude: -37.802353, longitude: 144.962223}]}
+        mode={"walking"}
+        optimizeWaypoints={true}
+        apikey={"AIzaSyDebH3jJ_9Z7i-22j9AQZuJYZG5apEJobc"}
+        strokeWidth={3}
+        strokeColor="hotpink"
       />
+        
+      </MapView>
     );
   }
 }
